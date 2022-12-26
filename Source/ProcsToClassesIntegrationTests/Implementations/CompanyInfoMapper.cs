fc6d81ada0aad375895647fd1915e3259c9008d4
Sergey Barskiy <sergey@barskiy.com>
2022-12-26 18:30:32 -0500

    update via tests

--- Source/ProcsToClassesIntegrationTests/Implementations/CompanyInfoMapper.cs
+++ Source/ProcsToClassesIntegrationTests/Implementations/CompanyInfoMapper.cs
@@ -13,6 +13,10 @@ namespace Classes
 			result.City = reader.IsDBNull(0) ? null : reader.GetString(0);
 			result.CompanyId = reader.GetInt32(1);
 			result.CompanyName = reader.GetString(2);
+			result.State = reader.IsDBNull(3) ? null : reader.GetString(3);
+			result.Street = reader.IsDBNull(4) ? null : reader.GetString(4);
+			result.Zip = reader.IsDBNull(5) ? null : reader.GetString(5);
+			result.RowVersionNumber = reader.GetInt32(6);
  
 			return result;
 		}
